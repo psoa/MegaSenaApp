7f92036fc28e86421710e3be3bac95ef8fe4fc03
psoa <tndias@gmail.com>
2018-06-05 23:04:58 -0300

    add onClick function to all play lottery buttons - should have another better way :(

--- app/src/main/java/br/com/psoa/megasena/megasenaapp/view/PlayLotteryActivity.kt
+++ app/src/main/java/br/com/psoa/megasena/megasenaapp/view/PlayLotteryActivity.kt
@@ -2,7 +2,13 @@ package br.com.psoa.megasena.megasenaapp.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.AppCompatButton
+import android.view.View
 import br.com.psoa.megasena.megasenaapp.R
+import kotlinx.android.synthetic.main.activity_play_lottery.*
+import android.widget.Toast
+
+
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -15,6 +21,28 @@ class PlayLotteryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_play_lottery)
+        cancel_button.setOnClickListener({ onCancelClicked() })
+        play_button.setOnClickListener({onPlayClicked()})
+    }
+
+    private fun onPlayClicked() {
+        //TODO: the magic happens here
+    }
+
+    private fun onCancelClicked() {
+        finish()
+    }
+
+    fun onPlayNumberClick(v: View) {
+        var bt = v as AppCompatButton
+        tToast("Go button ["+bt.text+"] clicked!")
+    }
+
+    private fun tToast(s: String) {
+        val context = applicationContext
+        val duration = Toast.LENGTH_LONG
+        val toast = Toast.makeText(context, s, duration)
+        toast.show()
     }
 
 }
